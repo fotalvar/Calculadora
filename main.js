0a211b6195985f925acd5e3ee6d0509822194424
Fede <119527971+fotalvar@users.noreply.github.com>
2023-03-08 17:31:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,133 +1,151 @@
-const display = document.querySelector('.display');
+const calculator = document.querySelector('.calculator');
+const exit = document.querySelector('.exit');
 const switchSignButton = document.querySelector('.switchSignButton');
 const eraseButton = document.querySelector('.eraseButton');
-const commaButton = document.querySelector('.commaButton');
+const messageBox = document.querySelector('.messageBox');
 const getNumbers = document.querySelectorAll('.numberButton');
 const getOperation = document.querySelectorAll('.operationButton');
-const calculator = document.querySelector('.calculator');
-const messageBox = document.querySelector('.messageBox');
-const exit = document.querySelector('.exit');
-let commaBlock = false;
+const equalButton = document.querySelector('.equalButton');
+const percentageButton = document.querySelector('.percentageButton');
+const commaButton = document.querySelector('.commaButton');
+const firstNumber = document.querySelector('.firstNumber');
+const secondNumber = document.querySelector('.secondNumber');
+const operator = document.querySelector('.operator')
 
 exit.addEventListener('click', () => {
     calculator.style.display = "none";
 });
 
 eraseButton.addEventListener('click', () => {
-    display.innerText = display.innerText.slice(0, -1);
-});
-
-switchSignButton.addEventListener('click', () => {
-    let number = display.innerText;
-    if (display.innerText === "") {
-        return;
-    }
-    if (number.charAt(0) === "-") {
-        number = number.slice(1);
-        display.innerText = number;
-        return;
-    } else {
-        number = "-" + number;
-        display.innerText = number;
-        return;
-    }
+    firstNumber.innerText = "";
+    secondNumber.innerText = "";
+    operator.innerText = "";
 });
 
 getNumbers.forEach(button => {
     button.addEventListener('click', event => {
-        if (display.innerText.endsWith("%")) {
+        if (firstNumber.innerText.length + secondNumber.innerText.length === 10) return;
+        if (operator.innerText === "") {
+            const clickedButton = event.target.textContent;
+            firstNumber.innerText += clickedButton;
         } else {
             const clickedButton = event.target.textContent;
-            display.innerText += clickedButton;
+            secondNumber.innerText += clickedButton;
         }
-    });
+    })
 });
 
 getOperation.forEach(button => {
     button.addEventListener('click', event => {
+        if (firstNumber.innerText === ("")) {
+            return;
+        }
         const clickedButton = event.target.textContent;
-        if (clickedButton === "=") {
-            return result();
-        }
-        if (display.innerText.length > 0 && !endsWithOperator(display.innerText)) {
-            display.innerText += clickedButton;
-            commaBlock = false;
-        }
+        operator.innerText = clickedButton;
     });
 });
 
-const endsWithOperator = (str) => {
-    return ["+", "-", "/", "*", "%", "."].some(op => str.endsWith(op));
-}
+equalButton.addEventListener('click', () => {
+    let firstNumberToOperate = firstNumber.innerText;
+    let secondNumberToOperate = secondNumber.innerText;
+    let operatorToOperate = operator.innerText;
+    let result;
+
+    if (firstNumber.innerText.endsWith(".") || secondNumber.innerText.endsWith(".")) return;
+    if (firstNumber.innerText === ("")) return;
+    if (firstNumber.innerText.length > 0 && secondNumber.innerText === ("")) return;
+
+    if (operatorToOperate === "+") {
+        result = parseFloat(firstNumberToOperate) + parseFloat(secondNumberToOperate);
+    } else if (operatorToOperate === "-") {
+        result = parseFloat(firstNumberToOperate) - parseFloat(secondNumberToOperate);
+    } else if (operatorToOperate === "*") {
+        result = parseFloat(firstNumberToOperate) * parseFloat(secondNumberToOperate);
+    } else if (operatorToOperate === "/") {
+        result = parseFloat(firstNumberToOperate) / parseFloat(secondNumberToOperate);
+        if (!isFinite(result)) {
+            showMessage();
+            return;
+        }
+    }
+
+    firstNumber.innerText = result;
+    secondNumber.innerText = "";
+    operator.innerText = "";
+
+});
+
+percentageButton.addEventListener('click', event => {
+    if (operator.innerText === "") {
+        firstNumber.innerText = parseFloat(firstNumber.innerText / 100);
+    } else {
+        secondNumber.innerText = parseFloat(secondNumber.innerText / 100);
+
+    }
+})
+
+switchSignButton.addEventListener('click', () => {
+    if (operator.innerText === "") {
+        number = firstNumber.innerText
+        if (number.charAt(0) === "-") {
+            number = number.slice(1);
+            firstNumber.innerText = number;
+            return;
+        } else {
+            number = firstNumber.innerText
+            number = "-" + number;
+            firstNumber.innerText = number;
+            return;
+        }
+    } else {
+        number = secondNumber.innerText
+        if (number.charAt(0) === "-") {
+            number = number.slice(1);
+            secondNumber.innerText = number;
+            return;
+        } else {
+            number = secondNumber.innerText
+            number = "-" + number;
+            secondNumber.innerText = number;
+            return;
+        }
+    }
+});
+
+commaButton.addEventListener('click', () => {
+    if (operator.innerText === "") {
+        let number = firstNumber.innerText;
+        let comma = number.includes(".")
+        if (comma) {
+            return;
+        } else {
+            number = number + ".";
+            firstNumber.innerText = number;
+        }
+    }
+    if (operator.innerText !== "") {
+        let number = secondNumber.innerText;
+        let comma = number.includes(".")
+        if (comma) {
+            return;
+        } else {
+            number = number + ".";
+            secondNumber.innerText = number;
+        }
+    }
+});
 
 const showMessage = () => {
     messageBox.style.visibility = 'visible';
+    messageBox.style.padding = '20px';
+    messageBox.style.opacity = '1';
+    messageBox.textContent = "ⓧ No se puede dividir por 0";
+    calculator.style.backgroundColor = '#ae6e6e';
     setTimeout(() => {
         messageBox.style.visibility = 'hidden';
-    }, 4000);
+        messageBox.style.padding = '2px';
+        messageBox.style.opacity = '0';
+        calculator.style = backgroundColor = '#7e808b'
+    }, 3000);
     return;
 };
-
-commaButton.addEventListener('click', () => {
-    if (display.innerText.length > 0 && commaBlock === false) {
-        if (endsWithOperator(display.innerText)) {
-        } else {
-            display.innerText += ".";
-            commaBlock = true;
-        }
-    }
-}
-);
-
-const removeZeros = () => {
-    display.innerText = display.innerText.replace(/\d+(\.\d+)?/g, function (match) {
-        return parseFloat(match).toFixed(3);
-    });
-}
-
-const result = () => {
-    if ((display.innerText).includes("%")) {
-        return calculateWithPercentage();
-    }
-    if (['+', '-', '*', '/', '.'].some(symbol => display.innerText.endsWith(symbol))) {
-        messageBox.textContent = " ⓧ No puede acabar tu operación con un símbolo. Modifícalo y vuelve a intentarlo."
-        showMessage();
-        return;
-    }
-    if (display.innerText.length > 0) {
-        let preCalculate = display.innerText
-        removeZeros()
-        let result = Function("return " + display.innerText)();
-        display.innerText = result;
-
-        if (!isFinite(result)) {
-            messageBox.textContent = "ⓧ No se puede dividir por 0"
-            showMessage();
-            return display.innerText = preCalculate;
-        } else {
-            display.innerText = result;
-            commaBlock = true;
-        }
-    }
-}
-
-const calculateWithPercentage = () => {
-    let numbers = display.innerText;
-    const regex = /\d+(?:\.\d+)?%/;
-    if (regex.test(numbers)) {
-        let numberWithoutPercentage = parseFloat(numbers.substring(0, numbers.length - 1));
-        numberWithoutPercentage = numberWithoutPercentage / 100;
-        display.innerText = numberWithoutPercentage;
-        return;
-    } else {
-        let percentage = numbers.match(/\d+%/)[0];
-        let symbol = numbers.match(/[-+*/]\d+%/)[0][0];
-        let percentageWithoutSymbol = (percentage.replace("%", "")) / 100;
-        let percentageWithSymbol = symbol + percentage;
-        let numberWithoutPercentage = numbers.replace(percentageWithSymbol, "");
-        let result = Function("return " + numberWithoutPercentage)();
-        let finalResult = Function("return " + (eval(result + symbol + (percentageWithoutSymbol * result))))();
-        commaBlock = true;
-        display.innerText = finalResult;
-    }
-}
